bb181ffa3a44905946bb52feef7cb526bc79f3dd
Alex Rousskov <rousskov@measurement-factory.com>
2015-04-16 14:17:34 -0600

    Polished comments.

--- Global.js
+++ Global.js
@@ -1,6 +1,4 @@
-/* Objects shared by many modules */
+/* Objects shared by many modules except Config which is in Config.js */
 
 import TypeMap from "./TypeMap";
 export let Types = new TypeMap();
-
-/* Config is in Config.js */
