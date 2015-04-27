0b54980c2de017f8ff400356a2e4ee80efba5d66
Alex Rousskov <rousskov@measurement-factory.com>
2015-04-26 23:46:14 -0600

    Added proxying mode configuration (reverse or forward, for now). Currently unused.

--- Config.js
+++ Config.js
@@ -7,3 +7,15 @@ export const OriginAddress = { // TODO: Make configurable
 	host: 'localhost',
 	port: 80,
 };
+
+export const ProxyingForward = "forward";
+export const ProxyingInReverse = "reverse";
+export const ProxyingMode = ProxyingForward; // TODO: Make configurable
+
+export function isForwardProxy() {
+	return ProxyingMode === ProxyingForward;
+}
+
+export function isReverseProxy() {
+	return ProxyingMode === ProxyingInReverse;
+}
