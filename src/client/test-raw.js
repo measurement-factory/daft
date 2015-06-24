4031dba8a98c7ef126c954923ba293b6fc712b76
Alex Rousskov <rousskov@measurement-factory.com>
2015-06-24 17:12:17 -0600

    Added "send my request" Client test case and polished README to mention Client and Server tools.

---
+++ src/client/test-raw.js
@@ -0,0 +1,29 @@
+import * as Global from "../misc/Global";
+import Transaction from "./Transaction";
+import RequestParser from "../http//RequestParser";
+import { Must } from "../misc/Gadgets";
+
+
+// An example of how to create a custom request from a string.
+// In most cases (including this example), it is actually better to initialize
+// just the relevant request parts. However, it is often convenient to just
+// cut-and-paste a real request from some log and be done with it.
+class MyTransaction extends Transaction {
+    constructor(userSocket, request) {
+		Must(!request);
+
+		let raw =
+`GET http://localhost/path|with|unwise|characters HTTP/1.1
+Host: localhost
+
+`;
+
+		let parser = new RequestParser();
+		parser.parse(raw);
+
+		Must(parser.message);
+		super(userSocket, parser.message);
+    }
+}
+
+Global.Types.setBase(Transaction, MyTransaction);
