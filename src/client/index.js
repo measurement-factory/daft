3156b3c990b722482c5fa2013c131c6259073018
Alex Rousskov <rousskov@measurement-factory.com>
2015-06-24 17:26:37 -0600

    Renamed test cases that drive agents to test "plots", primarily to distinguish these "scenarios" from built-in test cases.

--- src/client/index.js
+++ src/client/index.js
@@ -3,17 +3,17 @@
 
 /*
  * A driver script for the do-as-you-are-told HTTP server.
- * Runs a server configured with a test case specified on the command line.
+ * Runs a server configured with a test plot specified on the command line.
  */
 
 import Client from "./Agent";
 
 if (process.argv.length !== 3) {
-    throw `usage: ${process.argv[1]} <test_case.js5>`;
+    throw `usage: ${process.argv[1]} <test_plot.js5>`;
 }
 
 let fname = process.argv[2];
-console.log("Test case:", fname);
+console.log("Test plot:", fname);
 
 import fs from "fs";
 fs.readFile(fname, function (err, data) {
--- src/proxy/index.js
+++ src/proxy/index.js
@@ -2,16 +2,16 @@
 
 /*
  * A driver script for the do-as-you-are-told HTTP proxy.
- * Runs a proxy configured with a test case specified on the command line.
+ * Runs a proxy configured with a test plot specified on the command line.
  */
 
 import Proxy from "./Agent";
 
 if (process.argv.length !== 3)
-    throw `usage: ${process.argv[1]} <test_case.js5>`;
+    throw `usage: ${process.argv[1]} <test_plot.js5>`;
 
 let fname = process.argv[2];
-console.log("Test case:", fname);
+console.log("Test plot:", fname);
 
 import fs from "fs";
 fs.readFile(fname, function (err, data) {
