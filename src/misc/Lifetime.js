f27f40c9be5edebace705caffe7a9bbacbc13567
Alex Rousskov <rousskov@measurement-factory.com>
2016-01-11 16:03:57 -0700

    Support extending lifetime limit. Handy in multi-test scripts.

--- src/misc/Lifetime.js
+++ src/misc/Lifetime.js
@@ -1,19 +1,42 @@
 /* Manages program lifetime. */
 
+// The default 60s delay is meant to be long enough for most single tests.
+const Lifetime_ = 60*1000;
+
 let _GlobalTimeout = null;
 
-// Limits program lifetime by throwing an exception after the given delay.
-export function Limit(delay /* ms */) {
-    if (_GlobalTimeout !== null) {
-        console.log("Re-setting active global timeout.");
-        clearTimeout(_GlobalTimeout);
-    }
-    console.log(`Observing ${delay}ms global timeout.`);
+function _ClearTimeout() {
+    clearTimeout(_GlobalTimeout);
+    _GlobalTimeout = null;
+}
+
+function _SetTimeout(delay /* ms */) {
     _GlobalTimeout = setTimeout(function () {
         throw new Error(`Global ${delay}ms timeout.`);
     }, delay);
     _GlobalTimeout.unref(); // ignore if there are no other events left
 }
 
-// The default 60s delay is meant to be long enough for most single tests.
-Limit(60*1000);
+function _ResetTimeout(delay /* ms */, reason) {
+    if (_GlobalTimeout !== null) {
+        _ClearTimeout();
+        console.log(reason);
+    } else {
+        console.log(`Observing ${delay}ms global timeout.`);
+    }
+    _SetTimeout(delay);
+}
+
+// Limits program lifetime by throwing an exception after the given delay.
+// TODO: To export this function, subtract the lifetime already spent.
+// Otherwise, we are not limiting the lifetime but Extend()ing it.
+function Limit(delay /* ms */) {
+    _ResetTimeout(delay, `Resetting active global timeout to ${delay}ms.`);
+}
+
+// Extends previously set lifetime (or sets the first lifetime limit).
+export function Extend(delay = Lifetime_ /* ms */) {
+    _ResetTimeout(delay, `Extending active global timeout to ${delay}ms from now.`);
+}
+
+Limit(Lifetime_);
