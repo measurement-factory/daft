e292e82f61938c9ea0f4ccea4aadda0c9c80a91c
Alex Rousskov <rousskov@measurement-factory.com>
2016-01-15 10:54:04 -0700

    Temporary adding an eCAP Headman adapter (in an HTTP proxy host) test.
    Eventually, the Daft toolkit should only contain self-tests and perhaps
    test examples. Tests for other software should belong to that software.
    However, for now, while the APIs are still changing rapidly, and there
    are few toolkit users, it is easier to maintain all tests in one place.

---
+++ tests/test-headman.js
@@ -0,0 +1,64 @@
+/* Tests an HTTP proxy with a Headman eCAP adapter
+ * configured to hide and reveal empty Content-Type headers. */
+
+import ProxyCase from "./ProxyCase";
+import Body from "../src/http/Body";
+import * as Http from "../src/http/Gadgets";
+import * as Config from "../src/misc/Config";
+
+describe('Header-mangling proxy', function () {
+
+    let testCase = null;
+
+    beforeEach(function () {
+        testCase = new ProxyCase(this.currentTest.title);
+
+        /* force creation of all agents, even those we do not customize later */
+        testCase.client();
+        testCase.server();
+
+        testCase.check(() => {
+            Http.AssertForwardedMessage(testCase.client().transaction().request, testCase.server().transaction().request, "request");
+            Http.AssertForwardedMessage(testCase.server().transaction().response, testCase.client().transaction().response, "response");
+        });
+    });
+
+    it('should forward benign GET', async function () {
+        await testCase.run();
+    });
+
+    it('should forward benign POST', async function () {
+        testCase.client().request.startLine.method = 'POST';
+        testCase.client().request.addBody(new Body(Config.DefaultMessageBodyContent));
+        await testCase.run();
+    });
+
+    it('should handle empty Content-Type in response', async function () {
+        testCase.server().response.header.add("Content-Type", "");
+        await testCase.run();
+    });
+
+    it('should handle two empty Content-Type in response', async function () {
+        testCase.server().response.header.add("Content-Type", "");
+        testCase.server().response.header.add("Content-Type", " \t  ");
+        await testCase.run();
+    });
+
+    it('should handle mixed empty Content-Type in response', async function () {
+        testCase.server().response.header.add("Content-Type", "");
+        testCase.server().response.header.add("Content-Type", "foo");
+        testCase.server().response.header.add("Content-Type", " \t\r  ");
+        testCase.server().response.header.add("Content-Type", "bar");
+        await testCase.run();
+    });
+
+    it('should handle empty multi-line Content-Type in response', async function () {
+        testCase.server().response.header.add("Content-Type", " \t\r\n \t\r");
+        await testCase.run();
+    });
+
+    it('should handle strangely-cased empty Content-Type in response', async function () {
+        testCase.server().response.header.add("conTENt-tYpe", "");
+        await testCase.run();
+    });
+});
