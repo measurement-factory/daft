b0766719ca909b9e96c5f50072988044754a1799
Mark Simulacrum <mark.simulacrum@gmail.com>
2016-04-07 15:02:07 -0600

    Move replace into `let`.

--- src/http/one/MessageParser.js
+++ src/http/one/MessageParser.js
@@ -83,8 +83,7 @@ export default class MessageParser {
         header._raw = raw;
 
         // replace obs-fold with a single space
-        let rawH = raw;
-        rawH = rawH.replace(/\r*\n\s+/, ' ');
+        let rawH = raw.replace(/\r*\n\s+/, ' ');
 
         let rawFields = rawH.split('\n');
         Must(rawFields.length); // our caller requires CRLF at the headers end
