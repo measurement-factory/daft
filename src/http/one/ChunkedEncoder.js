ba747a43057f9b8b0ef9a63769aa7fef5fed7f2b
Alex Rousskov <rousskov@measurement-factory.com>
2020-11-13 14:49:37 -0500

    Made chunk size configurable via --chunk-size
    Also support withholding the last-chunk via --withhold-last-chunk. The
    sender is unaware of this withholding because we want the corresponding
    transaction to finish as if nothing happened (i.e. this is not a "pause
    at last-chunk" feature).
    
    TODO: All of this should be configurable for individual messages or even
    encoders and not just globally.

--- src/http/one/ChunkedEncoder.js
+++ src/http/one/ChunkedEncoder.js
@@ -5,11 +5,30 @@
 /* Wraps incoming data into chunked coding, one chunk per encode() call. */
 
 import Encoder from "./Encoder";
+import * as Config from "../../misc/Config";
 import { Must } from "../../misc/Gadgets";
+import assert from "assert";
+
+Config.Recognize([
+    {
+        option: "withhold-last-chunk",
+        type: "Boolean",
+        default: "false",
+        description: "when sending a chunked message, do not send last-chunk",
+    },
+    {
+        option: "chunk-size",
+        type: "Number",
+        default: "0",
+        description: "when sending a chunked message, limit payload bytes per chunk; " +
+            "0 sends all available data in one chunk"
+    },
+]);
 
 export default class ChunkedEncoder extends Encoder {
     constructor() {
         super("chunked");
+        assert(Config.ChunkSize >= 0);
     }
 
     encode(data) {
@@ -22,15 +41,36 @@ export default class ChunkedEncoder extends Encoder {
     finish(data) {
         // to ignore empty final data, we need to call our encode() but
         // to count last-chunk and trailer bytes, we need to call super.encode()
-        const final =
-            this.encode(data) + // last piece of data, if any
-            super.encode(""); // last-chunk followed by [empty] trailer
+        let result = this.encode(data); // the last piece of data, if any
+        if (!Config.WithholdLastChunk)
+            result += super.encode(""); // last-chunk followed by [empty] trailer
         super.finish("");
-        return final;
+        return result;
+    }
+
+    // empty data results in last-chunk
+    static EncodeOneChunk(data) {
+        return data.length.toString(16) + "\r\n" +
+            data + "\r\n";
     }
 
     _encode(data) {
-        return data.length.toString(16) + "\r\n" +
-            data + "\r\n";
+        if (!data.length)
+            return ChunkedEncoder.EncodeOneChunk(data); // last-chunk
+
+        if (!Config.ChunkSize)
+            return ChunkedEncoder.EncodeOneChunk(data); // all-in-one
+
+        let buf = '';
+        let pos = 0;
+        // configured-size chunks go first
+        while (pos + Config.ChunkSize <= data.length) {
+            buf += ChunkedEncoder.EncodeOneChunk(data.substring(pos, pos + Config.ChunkSize));
+            pos += Config.ChunkSize;
+        }
+        // any leftovers are sent in a smaller chunk
+        if (pos < data.length)
+            buf += ChunkedEncoder.EncodeOneChunk(data.substring(pos, data.length));
+        return buf;
     }
 }
