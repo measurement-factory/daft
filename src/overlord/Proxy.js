measurement-factory/daft

commit ad6b66aa5fe0b3219968e8c15e890e53f720c2a1

Support tests that want to cache responses larger
... than the default squid.conf allows. These changes appear to be
necessary, but I am not sure they are sufficient to cover all cases. For
example, while caching a `--body-size 1000000` (i.e. 1MB) response
response body works, caching a `--body-size 5000000` (i.e. 5MB) response
does not appear to work yet (with tests/cache-response.js).

src/overlord/Proxy.js
@@ -98,6 +98,7 @@ export class DutConfig {
             http_port 3128
             ${this._workersCfg()}
             ${this._collapsedForwardingCfg()}
+            ${this._anyCachingCfg()}
             ${this._memoryCachingCfg()}
             ${this._diskCachingCfg()}
             ${this._customCfg()}
@@ -131,15 +132,36 @@ export class DutConfig {
         return addresses;
     }
 
+    _anyCachingCfg() {
+        if (!this._memoryCaching && !this._diskCaching)
+            return;
+
+        // allow caching of responses configured to exceed default 4MB
+        // maximum_object_size but do not lower that limit below its default
+        const defaultObjectSizeMax = 64*1024*1024; // 4MB default
+        const maxResponseHeaderSize = 64*1024;
+        const maxResponseSize = maxResponseHeaderSize + Config.bodySize();
+        let cfg = ``;
+        if (maxResponseSize > defaultObjectSizeMax)
+            cfg += `maximum_object_size ${maxResponseSize} bytes\n`;
+
+        // Avoid "WARNING: disk-cache maximum object size is too large for
+        // mem-cache". TODO: The proxy should not warn about its own defaults!
+        // Also allow caching of responses configured to exceed default 512KB
+        // maximum_object_size_in_memory limit.
+        const defaultObjectSizeInMemoryMax = 512*1024; // 512KB default
+        const sizeToAllow = Math.max(defaultObjectSizeMax, maxResponseSize);
+        if (this._memoryCaching && sizeToAllow > defaultObjectSizeInMemoryMax)
+            cfg += `maximum_object_size_in_memory ${sizeToAllow} bytes`;
+
+        return this._trimCfg(cfg);
+    }
+
     _memoryCachingCfg() {
         const cacheSize = this._memoryCaching ? "100 MB" : "0";
         let cfg = `
             cache_mem ${cacheSize}
         `;
-        // Avoid "WARNING: disk-cache maximum object size is too large for mem-cache"
-        // TODO: The proxy should not warn about its own defaults!
-        if (this._memoryCaching)
-            cfg += `maximum_object_size_in_memory 4 MB`;
         return this._trimCfg(cfg);
     }
 

commit 50f59f68d65b7c44ae93f75f92074f47df31c9bb

Fix generated cacheless Squid configurations
    ...17:15:08| squid-overlord.conf(4): unrecognized: 'undefined'

Broken in commit ad6b66a.

src/overlord/Proxy.js
@@ -134,7 +134,7 @@ export class DutConfig {
 
     _anyCachingCfg() {
         if (!this._memoryCaching && !this._diskCaching)
-            return;
+            return '';
 
         // allow caching of responses configured to exceed default 4MB
         // maximum_object_size but do not lower that limit below its default
@@ -230,7 +230,7 @@ export class DutConfig {
     // makes cfg text pretty
     _trimCfg(cfg) {
         // TODO: Support rudimentary configuration parsing instead?
-        cfg = cfg.replace(/^\s+$/, ""); // remove leading empty line
+        cfg = cfg.replace(/^\s+$/mg, ""); // remove whitespace-only lines
         cfg = cfg.replace(/^\s+/, ""); // remove leading empty space
         cfg = cfg.replace(/\s+$/mg, ""); // remove trailing whitespace
         cfg = cfg.replace(/^\s{12}/mg, ""); // trim indentation