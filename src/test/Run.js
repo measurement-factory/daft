51fee554f9f22a52cbde519da072c59fac7cd038
Alex Rousskov <rousskov@measurement-factory.com>
2023-01-27 16:52:30 -0500

    Restart DUT for each test attempt; rework test concurrency
    When starting tests, Runner.js applied all test attempts to the same DUT
    instance. Also, each sequential attempt could have experienced a
    significantly different test concurrency level as threads with fewer
    necessary attempts would quit first.
    
        DUT start
        for_each thread in threads
            while more attempts are necessary
                test_object.run()
        DUT shutdown
    
    To reduce unknown differences between test attempts (both in terms of
    DUT state and in terms of concurrency levels), we now restart the DUT
    for each attempt and apply concurrency at each attempt level:
    
        while more attempts are necessary
            DUT start
            for_each thread in threads
                test_object.run()
            DUT shutdown
    
    Also established and polished related terminology (in src/test/Runner.js
    preamble).
    
    This change started with a failure to reproduce some old test results.
    The failing test (proxy-collapsed-forwarding) was known to be unstable
    and used 10 attempts by default to mitigate that flaw. I was thinking
    that something in the test environment had changed, resulting in those
    repeated attempts being less effective than before.
    
    I increased the number of attempts to at least 100, but that did not
    help. That outcome was puzzling -- an unstable but otherwise "working"
    test should succeed if retried "enough" times. In retrospect, I should
    have assumed that the old code being tested probably never passed this
    test, but I realized something else instead: Keeping the DUT running
    though multiple test attempts increases the difference between attempts
    and might prevent the test from succeeding! While differences in random
    "noise" may help an unstable test, non-random differences in DUT state
    (due to artifacts of earlier attempts) may also doom these repeated
    attempts (and complicate triage of individual attempts).
    
    Reproducible results, even negative ones, are better than having a
    chance of success: If a test reliably fails, it will be fixed or
    removed. Either is a much better outcome than a test that suddenly
    starts failing for no good reason. We should not make the DUT state more
    complex in hope that some poorly understood state change helps a lucky
    test attempt to succeed. CI tests should control as much as feasible,
    reducing inter-test dependencies, so that even an unstable test can
    "reliably" succeed when retried enough times.

--- src/test/Run.js
+++ src/test/Run.js
@@ -2,36 +2,55 @@
  * Copyright (C) 2015,2016 The Measurement Factory.
  * Licensed under the Apache License, Version 2.0.                       */
 
-/* A single test execution. */
-
 import assert from "assert";
 
+// Depending on the depth of detalization (see the setter methods), identifies
+// a single test, a single test attempt, or a single test thread execution.
+// See test/Runner.js for terminology.
 export default class Run {
 
-    constructor(runId, threadId) {
-        this.id = runId;
-        this.thread = threadId;
-        this.attempt_ = null; // optional, set by attempt()
+    constructor(testId) {
+        assert.strictEqual(arguments.length, 1);
+        this._testId = testId;
+        this._thread = null; // see setThread()
+        this._attempt = null; // see setAttempt()
     }
 
-    attempt(n) {
-        assert(n !== undefined);
-        assert(n !== null);
-        this.attempt_ = n;
+    clone() {
+        const copy = new Run(this._testId);
+        copy._thread = this._thread; // may be null or zero
+        copy._attempt = this._attempt; // may be null or zero
+        return copy;
+    }
+
+    setThread(threadId, expectedThreadCount) {
+        assert.strictEqual(arguments.length, 2);
+        assert(threadId > 0);
+        this._thread = (expectedThreadCount > 1) ? threadId : 0;
+    }
+
+    setAttempt(attemptId, retriesAllowed) {
+        assert.strictEqual(arguments.length, 2);
+        assert(attemptId > 0);
+        this._attempt = retriesAllowed ? attemptId : 0;
     }
 
     toString() {
-        let description = "test run#" + this.id;
-        if (this.attempt_)
-            description += "+" + this.attempt_;
+        let description = "test run#" + this._testId;
+        if (this._attempt)
+            description += "." + this._attempt;
+        if (this._thread)
+            description += "(" + this._thread + ")";
         return description;
     }
 
     // useful for DNS labels and other IDs with restrictive character sets
     toWord() {
-        let word = this.id;
-        if (this.attempt_)
-            word += "a" + this.attempt_;
+        let word = this._testId;
+        if (this._thread)
+            word += "t" + this._thread;
+        if (this._attempt)
+            word += "a" + this._attempt;
         return word;
     }
 }
