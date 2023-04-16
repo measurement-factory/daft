d4b2c633a39e635d38d012d99e286245d50996ca
Alex Rousskov <rousskov@measurement-factory.com>
2023-04-16 17:28:12 -0400

    Use random numbers to generate text longer than 32 bytes
    I removed the old _LongText optimization because I was concerned that
    (Range handling) tests relying on comparison of body parts were too easy
    to pass accidentally since many body parts, even larger ones, were
    identical.
    
    My quick tests did not show significant performance impact of the
    now-removed _LongText optimization, even when using 100KB+ responses,
    that are fairly rare in current tests. We may come back to this if
    performance does become a problem.
    
    I did consider using crypto.randomBytes() and friends instead but
    decided not not use crypto for two reasons:
    
    * Scary documentation mentioning a thread-based implementation that may
      affect performance. Some say those worries are outdated, but I could
      not quickly find a direct confirmation that node documentation is
      stale in this regard.
    
    * We may want to control the generation _seed_ someday. In that case,
      migrating from Math.random() to a custom r.n.g. might be easier than
      using crypto.

--- src/misc/Gadgets.js
+++ src/misc/Gadgets.js
@@ -190,8 +190,6 @@ export function ReceivedBytes(socket, bytes, description /*, logPrefix*/) {
     console.log(toLog);
 }
 
-const _LongText = "TeXt".repeat(10240/4);
-
 // Returns a length-bytes string, including the prefix.
 // Besides the given prefix, the rest of the string is base-36 encoded.
 // Does not guarantee uniqueness, even within the same process.
@@ -202,17 +200,9 @@ export function RandomText(prefix, length) {
     let buf = "";
 
     // random header (for all generated texts with short-enough prefixes)
-    while (buf.length < length && buf.length < 32) {
+    while (buf.length < length) {
         buf += Math.random().toString(36).substr(2); // remove leading "0."
     }
-
-    // fast-growing body from fixed tokens (if needed)
-    if (buf.length < length) {
-        const remainingChars = length - buf.length;
-        buf += _LongText.repeat(remainingChars/_LongText.length + 1);
-    }
-
-    assert(buf.length >= length);
 
     // remove any extra trailing characters
     return (prefix + buf).substr(0, length);
